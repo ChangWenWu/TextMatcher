25795ad3117c44aa1044ff2728f8896b943b33d7
changwenwu <wucw5513@mails.jlu.edu.cn>
2019-10-16 20:59:38 +0800

    读取TXT文本

--- test-matcher/src/App.js
+++ test-matcher/src/App.js
@@ -5,7 +5,7 @@ import { textMatcher } from './utils/textMatcher';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { value: '' ,textResult : [],position:[]};
+    this.state = { resource: [], value: '' ,textResult : [],position:[]};
   }
 
   handleChange = (event) => {
@@ -18,11 +18,32 @@ class App extends Component {
     });
   }
 
+  selectFile = (event) => {
+    var selectedFile = event.target.files[0];
+    var name = selectedFile.name;
+    var size = selectedFile.size;
+    console.log("文件名:"+name+"大小："+size);
+    var reader = new FileReader();
+    reader.readAsText(selectedFile);
+
+    reader.onload = () =>{
+      const strArray = reader.result.split("\n");
+      console.log(strArray);
+      this.setState({
+        resource: strArray,
+      });
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <div className="test-data">
           {/* <FlavorForm /> */}
+        </div>
+        <div className="test-file">
+          <p>请选择文件</p>
+          <input type="file" onChange={this.selectFile}/>
         </div>
         <div className="test-matcher">
           <p>请输入匹配字符</p>
